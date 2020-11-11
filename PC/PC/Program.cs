f4fd9f629de0744a73a0494e0ab9f2d6806eb244
Minh Châu <minhchau.music1999@gmail.com>
2020-11-11 11:31:14 +0700

    new

--- PC/PC/Program.cs
+++ PC/PC/Program.cs
@@ -16,7 +16,7 @@ namespace PC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new dangnhap());
         }
     }
 }
